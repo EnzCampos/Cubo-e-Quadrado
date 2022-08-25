2afe110dadb7ae4eb408da4cd96dfdf92d770cfe
Enzo Henrique Ferreira Campos <98186034+EnzCampos@users.noreply.github.com>
2022-08-25 19:46:37 -0300

    Adicionando o raciocínio de resolução.

--- cuboQuadrado.js
+++ cuboQuadrado.js
@@ -1,5 +1,10 @@
 function cuboQuadrado(numA, numB) {
-    //Calcula os cubos dos limites superior e inferior para definir o intervalo de checagem.
+    
+    // Calcular os cubos dos limites superior e inferior para definir o intervalo de checagem. 
+    // Ao invés de calcular número por número podemos calcular apenas os números inteiros que são raízes cúbicas entre os limites.
+    // Por exemplo, entradas: 64, 729, raizes cubicas dos limites: 4 e 9, intervalo a ser verificado: 4, 5, 6, 7, 8, 9, verificar quais números
+    // tem raizés quadradas ou seja 4 e 9, por tanto o resultado seria 2.
+    
     const limiteInferior = Math.ceil(Math.cbrt(numA)); //Math.ceil para arredondar o valor resultante para cima pois é um limite inferior.
     const limiteSuperior = Math.floor(Math.cbrt(numB)); //Math.floor para arredondar o valor resultante para baixo pois é um limite superior.
     
